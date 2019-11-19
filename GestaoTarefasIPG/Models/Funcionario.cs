c9b452b630f253c0bd43313ac57c6d31392e8d05
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2019-11-19 12:13:18 +0000

    Criado modelo para funcionarios

---
+++ GestaoTarefasIPG/Models/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models {
+    public class Funcionario {
+        [Required]
+        public int FuncionarioId { get; set; }
+        [Required]
+        public string nome { get; set; }
+        [Required]
+        [RegularExpression("/9[1236][0-9]{7}|2[1-9]{1,2}[0-9]{7}/")]
+        public string numeroTelemovel { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string setor { get; set; }
+        [Required]
+        public string numeroFuncionario { get; set; }
+    }
+}
