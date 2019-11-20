7d51de42b74361d65054f05b6ceeb34028cb3abc
Nil Silva <nil_silva_12@outlook.com>
2019-11-20 17:32:02 +0000

    Criado modelo para professores.

---
+++ GestaoTarefasIPG/Models/Professor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class Professor
+    {
+        [Required]
+        public int ProfessorId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [RegularExpression("/9[1236][0-9]{7}|2[1-9]{1,2}[0-9]{7}/")]
+        public string NumeroTelemovel { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public int Gabinete { get; set; }
+    }
+}
