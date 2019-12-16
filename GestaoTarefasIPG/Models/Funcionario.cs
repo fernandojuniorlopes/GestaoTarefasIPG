a6a492239bbb5a98dfc5a00860fe7e408ba2fd7d
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2019-12-16 22:34:10 +0000

    Alterações no modelo Funcionario

--- GestaoTarefasIPG/Models/Funcionario.cs
+++ GestaoTarefasIPG/Models/Funcionario.cs
@@ -7,9 +7,11 @@ using System.Threading.Tasks;
 
 namespace GestaoTarefasIPG.Models {
     public class Funcionario {
+
         [Required]
         public int FuncionarioId { get; set; }
         [Required]
+        [StringLength(50, ErrorMessage = "{0} deve ter entre {2} a {1} caracteres", MinimumLength = 5)]
         public string Nome { get; set; }
         [Required]
         [RegularExpression("(9[1236][0-9]{7})|(2[1-9][0-9]{7})")]
@@ -18,6 +20,8 @@ namespace GestaoTarefasIPG.Models {
         [EmailAddress]
         public string Email { get; set; }
         [Required]
+        [Display(Name = "Número de Funcionario")]
+        [Range(0,10000, ErrorMessage = "{0} deve estar contido entre {1} e {2}.")]
         public string NumeroFuncionario { get; set; }
     }
 }
